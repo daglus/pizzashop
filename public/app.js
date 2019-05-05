73f781212f073210e89a766fcd4d1f3617da82dc
daglus <daglus.ck.ua@gmail.com>
2019-05-05 21:42:19 +0300

    Add id to product, when put it to cart

--- public/app.js
+++ public/app.js
@@ -13,7 +13,7 @@ function add_to_cart(id)
 {
   var key = 'product_' + id;
 
-	var x = window.localStorage.getItem(key);
+  var x = window.localStorage.getItem(key);
   x = x * 1 + 1;
   window.localStorage.setItem(key, x);
 }
