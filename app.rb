1e1f92ab4023863ace0dddd4b16a054fc4d66669
daglus <daglus.ck.ua@gmail.com>
2019-05-07 14:49:03 +0300

    Add cart view

--- app.rb
+++ app.rb
@@ -21,27 +21,26 @@ end
 post '/cart' do
 	orders_input = params[:orders]
 	@orders = parse_orders_input orders_input
-	erb "Hello #{@orders.inspect}"
+	
+	erb :cart
 end
 
 def parse_orders_input orders_input
 
-	s1 = orders_input.split(/,/)
+  s1 = orders_input.split(/,/)
 
-	arr = []
+  arr = []
 
-	s1.each do |x|
-		s2 = x.split(/\=/)
+  s1.each do |x|
+    s2 = x.split(/=/)
+    s3 = s2[0].split(/_/)
 
-		s3 = s2[0].split(/_/)
+    id = s3[1]
+    cnt = s2[1]
 
-		id = s3[1]
-		cnt = s2[1]
+    arr2 = [id, cnt]
+    arr.push arr2
+  end
 
-		arr2 = [id, cnt]
-
-		arr.push arr2
-	end
-
-	return arr
+  return arr
 end
