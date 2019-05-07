17716177bcfed57d6cc758f19b2c49df34304f00
daglus <daglus.ck.ua@gmail.com>
2019-05-07 15:44:07 +0300

    Add form to cart

--- app.rb
+++ app.rb
@@ -19,9 +19,9 @@ get '/about' do
 end
 
 post '/cart' do
-  orders_input = params[:orders]
+  @orders_input = params[:orders]
 
-  @items = parse_orders_input orders_input
+  @items = parse_orders_input @orders_input
 
   @items.each do |item|
     item[0] = Product.find(item[0])
